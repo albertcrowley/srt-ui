9f1de4912498e863fd66bc3d875485c4e76ecd5f
Al Crowely <albert.crowley@tcg.com>
2019-09-24 10:24:34 -0400

    moving prod server name

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  SERVER_URL: 'https://srt-server.app.cloud.gov/api',
+  SERVER_URL: 'https://srt-server-prod.app.cloud.gov/api',
   EMAIL_NOTIFICATION: true,
-  FILE_UPLOAD_API: 'http://srt-server.app.cloud.gov/file/upload',
+  FILE_UPLOAD_API: 'http://srt-server-prod.app.cloud.gov/file/upload',
   needToken: false
 
   // SERVER_URL: 'http://localhost:3000',
